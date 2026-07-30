c2f1db2eb1100db3707c47c9c8c5251fda52b0b4
Gpeder <gustavoapeder@gmail.com>
2026-07-30 13:50:46 -0300

    componentes shadcn instalados e ajustados

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,6 @@
 function App() {
   return (
-    <main className="min-h-screen bg-background px-6 py-10 text-foreground">
-      <p className="text-lg">Teste</p>
-    </main>
+    <main className="min-h-screen bg-background px-6 py-10 text-foreground"></main>
   );
 }
 
