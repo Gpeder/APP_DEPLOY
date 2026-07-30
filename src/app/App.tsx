2bc14bffcfac021c60e18c0fd54d3ecc0f33a847
Gpeder <gustavoapeder@gmail.com>
2026-07-30 13:35:22 -0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-background px-6 py-10 text-foreground">
+      <p className="text-lg">Teste</p>
+    </main>
+  );
+}
+
+export default App;
